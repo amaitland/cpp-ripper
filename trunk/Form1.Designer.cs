d22a18c1bd64ae352eef64be62ad72b110c1e39e
cdiggins <cdiggins@3dcb93b4-9be2-11de-bc28-6fc548ead51f>
2009-09-08 11:33:39 +0000

    First upload.

---
+++ trunk/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CppRipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.editOutput = new System.Windows.Forms.RichTextBox();
+            this.editMain = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 253);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(349, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.editMain);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.editOutput);
+            this.splitContainer1.Size = new System.Drawing.Size(349, 253);
+            this.splitContainer1.SplitterDistance = 170;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // editOutput
+            // 
+            this.editOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editOutput.Location = new System.Drawing.Point(0, 0);
+            this.editOutput.Name = "editOutput";
+            this.editOutput.Size = new System.Drawing.Size(349, 79);
+            this.editOutput.TabIndex = 0;
+            this.editOutput.Text = "";
+            // 
+            // editAllNodes
+            // 
+            this.editMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editMain.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editMain.Location = new System.Drawing.Point(0, 0);
+            this.editMain.Name = "editAllNodes";
+            this.editMain.Size = new System.Drawing.Size(349, 170);
+            this.editMain.TabIndex = 2;
+            this.editMain.Text = "";
+            this.editMain.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 275);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox editOutput;
+        private System.Windows.Forms.RichTextBox editMain;
+    }
+}
+
---
+++ trunk/Parser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CppRipper
+{
+    public class CppFileParser
+    {
+        static CppStructuralGrammar grammar = new CppStructuralGrammar();
+
+        private string message;
+
+        public string Message { get { return message; } }
+
+        public CppFileParser(IAstPrinter printer, string file)
+        {
+            Rule parse_rule = grammar.file;
+
+            string text = File.ReadAllText(file);
+            printer.Clear();
+            ParserState state = new ParserState(text);
+
+            try
+            {
+                if (!parse_rule.Match(state))
+                    message = "Failed to parse file " + file;
+                else
+                    message = "Successfully parsed file";
+            }
+            catch (ParsingException e)
+            {
+                state.ForceCompletion();
+                message = e.Message;
+            }
+
+            printer.PrintNode(state.GetRoot(), 0);
+        }
+    }
+
+    public class CppFileSetParser
+    {
+        public DirectoryInfo di;
+
+        public CppFileSetParser(IAstPrinter printer, string sDir)
+        {
+            di = new DirectoryInfo(sDir);
+            foreach (FileInfo fi in di.GetFiles("*.c;*.cpp;*.h;*.hpp;"))
+            {
+                CppFileParser fp = new CppFileParser(printer, fi.Name);
+            }
+        }
+    }
+}
